5824817a3bbb0fbc2eaf394edfc7a39ecc83ae43
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2026-01-06 17:59:15 +0100

    first commit

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
-import { DUMMY_USERS } from '../../dummy-users';
 
 @Injectable({
   providedIn: 'root',

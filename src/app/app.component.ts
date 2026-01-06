ae5a2488c47d44b99c7b316889e32122edad1380
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2026-01-06 15:15:48 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { UsersComponent } from "./users/users.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, UsersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
